8cce2fada623ccf8144a85c62474279d689dd6f1
ArthurRFr <arthur_fronza2011@hotmail.com>
2021-05-08 10:00:29 -0300

    Aulas Básicas Realizadas

---
+++ src/CreateCourseService.ts
@@ -0,0 +1,17 @@
+
+
+interface Course {
+    name     : string;
+    duration?: number;
+    educator : string;
+}
+
+class CreatCourseService {
+
+    execute({ name, duration = 8, educator }: Course) {
+        console.log(name, duration, educator);
+    }
+
+}
+
+export default new CreatCourseService();
